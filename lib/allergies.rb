5df7b18adc8d46eb5823741b1dddf16be8bdf2d5
Julia Dickey and Katarina Tuttle <julia+katarina@>
2016-06-30 16:43:43 -0700

    Add back end logic for up to 2 allergens

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -2,6 +2,18 @@ require('pry')
 
 class Fixnum
   define_method(:allergies) do
-
+    all_scores = {128 => "cats", 64 => "pollen", 32 => "chocolate", 16 => "tomatoes", 8 => "strawberries", 4 => "shellfish", 2 => "peanuts",1 => "eggs"}
+    score_array = all_scores.keys()
+    allergies = []
+    user_number = self
+    score_array.each() do |number|
+      number_decreasing = user_number
+      if (number_decreasing./(number)).floor()>= 1
+        allergies.push(all_scores.fetch(number))
+        number_decreasing = (number_decreasing -= number)
+binding.pry
+      end
+    end
+    allergies.join(", ")
   end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -1,10 +1,13 @@
-require('anagrams')
+require('allergies')
 require("rspec")
 require("sinatra")
 require('capybara/rspec')
 
 describe('Fixnum#allergies') do
+  # it("returns a list of allergies dependeng on user's allergy score") do
+  #   expect(3.allergies()).to(eq("peanuts, eggs"))
+  # end
   it("returns a list of allergies dependeng on user's allergy score") do
-    expect(3.allergies()).to(eq("eggs, peanuts"))
+    expect(240.allergies()).to(eq("cats, pollen, chocolate, tomatoes"))
   end
 end
